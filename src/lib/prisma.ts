5686b65c9f9a70855d8fd735f9fc35204620b3a5
Angga Dhammika Jaya <anggadj94@gmail.com>
2026-06-16 18:29:02 +0700

    Try Deployment

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,9 @@
-import { PrismaClient } from '../generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 
-// Ambil URL dari .env
-const connectionString = `${process.env.DIRECT_URL}`;
+// WAJIB pakai DATABASE_URL (Pooler) untuk aplikasi Next.js
+const connectionString = `${process.env.DATABASE_URL}`;
 
 // Buat connection pool menggunakan pg
 const pool = new Pool({ connectionString });
